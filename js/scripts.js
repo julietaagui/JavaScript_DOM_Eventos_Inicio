072b0f32c9752e98e8499514bae4567ec1b484c1
Julieta Aguilera <aguilerajulieta700@gmail.com>
2024-01-26 18:19:43 -0300

    se agan archivos editados de un nuevo proyecto

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// querySelector (retorna en ninguno o hasta un elemento que concuerde con el selector)
+
+// .clase
+// #id
+const heading = document.querySelector('.header__texto h2'); //retorna a 0 o un elemento
+heading.textContent = 'Nuevo heading';
+// heading.classList.add('nueva-clase'); para agregar clases
+console.log(heading);
+
+
+
+// querySelectorAll(llama a toodos los que correspondan)
+
+const enlaces = document.querySelectorAll('.navegacion a');
+console.log(enlaces[0]);
+
+enlaces[0].textContent = 'Nuevo Texto para Enlace';
+console.log(enlaces)
+
+
+
+
+// getElementById
